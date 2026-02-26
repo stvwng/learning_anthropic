effa1e0d3db113835768c4c310a803df7f9164b0
Steve Wang <stevewang@alumni.stanford.edu>
2026-02-25 21:34:09 -0500

    Tools to read and edit document

--- cli_project/mcp_server.py
+++ cli_project/mcp_server.py
@@ -1,5 +1,6 @@
 from mcp.server.fastmcp import FastMCP
-
+from pydantic import Field
+# Create an MCP server
 mcp = FastMCP("DocumentMCP", log_level="ERROR")
 
 
@@ -12,8 +13,33 @@ docs = {
     "spec.txt": "These specifications define the technical requirements for the equipment.",
 }
 
-# TODO: Write a tool to read a doc
-# TODO: Write a tool to edit a doc
+# the mcp.tool decorator is used to define a tool that can be used by the MCP server.
+# It automatically generates a JSON schema for the tool and registers it with the MCP server.
+@mcp.tool(
+    name="read_doc_contents",
+    description="Read the contents of a document and return it as a string."
+)
+def read_document(
+    doc_id: str = Field(description="Id of the document to read")
+):
+    if doc_id not in docs:
+        raise ValueError(f"Doc with id {doc_id} not found")
+    
+    return docs[doc_id]
+
+@mcp.tool(
+    name="edit_document",
+    description="Edit a document by replacing a string in the documents content with a new string."
+)
+def edit_document(
+    doc_id: str = Field(description="Id of the document that will be edited"),
+    old_str: str = Field(description="The text to replace. Must match exactly, including whitespace."),
+    new_str: str = Field(description="The new text to insert in place of the old text.")
+):
+    if doc_id not in docs:
+        raise ValueError(f"Doc with id {doc_id} not found")
+    
+    docs[doc_id] = docs[doc_id].replace(old_str, new_str)
 # TODO: Write a resource to return all doc id's
 # TODO: Write a resource to return the contents of a particular doc
 # TODO: Write a prompt to rewrite a doc in markdown format
