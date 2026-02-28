c195d04debb46c50cd75bc193cf7e5488b3bdedd
Steve Wang <stevewang@alumni.stanford.edu>
2026-02-27 21:00:00 -0500

    Code for MCP client to read resource from MCP server

--- cli_project/mcp_client.py
+++ cli_project/mcp_client.py
@@ -4,7 +4,8 @@ from typing import Optional, Any
 from contextlib import AsyncExitStack
 from mcp import ClientSession, StdioServerParameters, types
 from mcp.client.stdio import stdio_client
-
+import json
+from pydantic import AnyUrl
 
 class MCPClient:
     def __init__(
@@ -58,10 +59,16 @@ class MCPClient:
         # TODO: Get a particular prompt defined by the MCP server
         return []
 
+    # read a resource from the MCP server, parse the contents and return it
     async def read_resource(self, uri: str) -> Any:
-        # TODO: Read a resource, parse the contents and return it
-        return []
-
+        result = await self.session().read_resource(AnyUrl(uri))
+        resource = result.contents[0]
+        if isinstance(resource, types.TextResourceContents):
+            if resource.mimeType == "application/json":
+                return json.loads(resource.text)
+            
+        return resource.text
+    
     async def cleanup(self):
         await self._exit_stack.aclose()
         self._session = None
